bcd02ffbff6e25ebbc0d4246cc7d473c1556cea0
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-02-26 22:03:22 -0500

    thresholdReached() added and checked

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -31,4 +31,23 @@ public class LogginLabTest {
             }
         }
     }
+
+    @org.junit.Test
+    public void thresholdReached() {
+        Integer finalLimit = 5;
+        Integer overClocked= 6;
+
+        LogginLab lab = new LogginLab();
+        lab.setThreshold(finalLimit);
+
+        for (Integer i = 1; i <= (overClocked); i++) {
+            if (lab.thresholdReached(i)) {
+                logger.log(Level.INFO, "Threshold EXCEEDED!");
+                assertTrue(lab.thresholdReached(i));
+            } else {
+                logger.log(Level.INFO, "Limit currently at " + i + ". Good to go.");
+                assertFalse(lab.thresholdReached(i));
+            }
+        }
+    }
 }
